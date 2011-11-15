bd60399577cf89525b8ff2b40c62c977a319371c
David Greenberg <david.greenberg@twosigma.com>
2011-11-15 18:08:26 +0000

    Added async job to client
    Logs are no longer delayed--they're written immediately. Also, the overridable kill function now takes no arguments
    
    Implemented asynchronous job mixin for running on hosts
    
    Changed nuke_subprocess to a plain os.kill() to avoid the sleep(1) in nuke_subprocess
    
    Signed-off-by: David Greenberg <dsg123456789@gmail.com>

--- server/hosts/ssh_host.py
+++ server/hosts/ssh_host.py
@@ -242,3 +242,53 @@ class SSHHost(abstract_ssh.AbstractSSHHost):
                 self.ssh_ping()
             except error.AutoservSshPingHostError:
                 self.setup_ssh_key()
+
+class AsyncSSHMixin(object):
+    def __init__(self, *args, **kwargs):
+        super(AsyncSSHMixin, self).__init__(*args, **kwargs)
+
+    def run_async(self, command, stdout_tee=None, stderr_tee=None, args=(),
+                  connect_timeout=30, options='', verbose=True,
+                  stderr_level=utils.DEFAULT_STDERR_LEVEL,
+                  cmd_outside_subshell=''):
+        """
+        Run a command on the remote host. Returns an AsyncJob object to
+        interact with the remote process.
+
+        Sorry, but you can't use stdin--that's how we do job control.
+
+        This is mostly copied from SSHHost.run and SSHHost._run
+        """
+        if verbose:
+            logging.debug("Running (async ssh) '%s'" % command)
+
+        # Start a master SSH connection if necessary.
+        self.start_master_ssh()
+
+        env = " ".join("=".join(pair) for pair in self.env.iteritems())
+
+        ssh_cmd = self.ssh_command(connect_timeout, options)
+        if not env.strip():
+            env = ""
+        else:
+            env = "export %s;" % env
+        for arg in args:
+            command += ' "%s"' % utils.sh_escape(arg)
+        full_cmd = '{ssh_cmd} "{env} {cmd} {killer_bits}"'.format(
+            ssh_cmd=ssh_cmd, env=env,
+            cmd=utils.sh_escape("%s (%s)" % (cmd_outside_subshell, command)),
+            killer_bits=utils.sh_escape("& read; kill -9 $!"))
+
+        job = utils.AsyncJob(full_cmd, stdout_tee=stdout_tee,
+                              stderr_tee=stderr_tee, verbose=verbose,
+                              stderr_level=stderr_level,
+                              stdin=subprocess.PIPE)
+
+        def kill_func():
+            #this triggers the remote kill
+            os.write(job.sp.stdin.fileno(), "lulz\n")
+            utils.nuke_subprocess(job.sp)
+
+        job.kill_func = kill_func
+
+        return job
